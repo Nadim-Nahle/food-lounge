325d37c3d3f8e8caeba075a682d8fc8a55f3132b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-05-31 22:48:29 +0300

    fix - adjust api url path

--- js/user_list.js
+++ js/user_list.js
@@ -1,18 +1,16 @@
 axios({
   method: "get",
-  url: "http://localhost/Foodlounge/get_users.php",
+  url: "localhost/backend/php/get_users.php",
 }).then(function (response) {
-  console.log(response.data[0]["F_Name"]);
+  console.log(response.data[0]["username"]);
   document.getElementById("listtable").innerHTML =
-    " <tr><th>username</th><th>email</th><th>bio</th></tr>";
+    " <tr><th>username</th><th>email</th></tr>";
   for (var i = 0; i < response.data.length; i++) {
     document.getElementById("listtable").innerHTML +=
       "<tr><td>" +
       response.data[i]["username"] +
       "</td><td>" +
       response.data[i]["email"] +
-      "</td><td>" +
-      response.data[i]["bio"] +
       "</td></tr>";
   }
 });
